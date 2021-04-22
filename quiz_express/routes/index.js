ee8797886a745d16100cd9e01d603be5d46a026a
ClaudiaQuezadaRiquelme <ca.quezada.riquelme@gmail.com>
2021-04-21 23:19:28 -0400

    implementación según guía screencast

---
+++ quiz_express/routes/index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+
+const quizController = require('../controllers/quiz');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+/* GET /credits */
+router.get('/credits', function(req, res, next) {
+  res.render('credits');
+});
+
+/* GET /quizzes */
+router.get('/quizzes', quizController.index);
+
+module.exports = router;
